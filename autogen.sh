231296b4f3f831fd8c672ed80e0e0e265b8b221a
Adam Lackorzynski <adam@l4re.org>
2025-02-22 11:43:49 +0100

    autogen: Simplify
    Just call "autoreconf -fi".

--- autogen.sh
+++ autogen.sh
@@ -2,15 +2,11 @@
 #
 # $Id: autogen.sh,v 1.16 2009-11-15 20:00:56 al-guest Exp $
 
-AUTOMAKEVER=1.16
-
 set -x
 
-aclocal-$AUTOMAKEVER
-[ "$?" != 0 ] && echo "aclocal-$AUTOMAKEVER not available or failed!" && exit 1
-autoheader || exit 1
-automake-$AUTOMAKEVER -c --add-missing --force --gnu || exit 1
-autoconf || exit 1
+autoreconf -fi
 
 # remove once it comes via config.sub directly
-perl -p -i -e 's/(\| hcos\* )/$1| l4re* /' config.sub
+if grep -qv l4re config.sub; then
+  perl -p -i -e 's/(\| hcos\* )/$1| l4re* /' config.sub
+fi
